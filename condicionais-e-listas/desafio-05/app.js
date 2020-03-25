3061352b0ef7724e93a66776e0f45d8fe5e4ad75
leonardo <leonardo.ruma@gmail.com>
2020-03-24 22:57:55 -0300

    condicionais e listas com V-if, V-else-if, v-else, v-for, v-show, templates..

---
+++ condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,18 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		expressao: true,
+		nomes: ['lucas', 'amanda', 'fofinha'],
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livros: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		alunos: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		}
+	}
+});
