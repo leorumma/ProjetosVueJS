db4c44028a87e9bb9dd985f690b1cd8068611dd9
leonardo <leonardo.ruma@gmail.com>
2020-03-27 11:26:15 -0300

    introducao componentes

---
+++ componentes-intro/src/contadores.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="contadores">
+        <h1>contadores</h1>
+        <app-contador :key="c" v-for="c in 5" />
+    </div>
+
+</template>
+<script>
+import contador from './contador.vue'
+
+export default {
+    components:{
+        'app-contador': contador
+    }
+}
+</script>
+<style>
+
+</style>
